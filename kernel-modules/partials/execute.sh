ac75f921dddc7cd0d258983e9ba47f96be3d5831
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2020-05-10 20:37:47 +0200

    add external-dns (#1)

--- kernel-modules/partials/execute.sh
+++ kernel-modules/partials/execute.sh
@@ -5,4 +5,5 @@ configdir=$(dirname $0)
 mkdir -p /var/lib/rancher/k3os/config.d
 cp "${configdir}"/*.yaml /var/lib/rancher/k3os/config.d/
 sleep 10
+# TODO: find out why k3os config doesn't detect any changes
 k3os config
