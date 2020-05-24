25ffc3d5c59787e0361360d63c7c730c737b440a
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2020-05-24 21:13:06 +0200

    Vault + Rook - missing kernel module rbd

--- rook/clean.sh
+++ rook/clean.sh
@@ -2,7 +2,11 @@
 set -xeEuo pipefail
 
 hosts=(k3os-rpi-{0,1,2})
-modules=(block-storage cluster toolbox operator common)
+if [[ "${1:-}" == "--all" ]] ; then
+  modules=(block-storage cluster toolbox operator common)
+else
+  modules=()
+fi
 
 for dir in "${modules[@]}"; do
   pushd "$dir"
@@ -12,5 +16,7 @@ done
 
 for host in "${hosts[@]}"; do
   ssh "$host" sudo rm -rf /var/lib/rook || true
+  ssh "$host" sudo dmsetup remove_all || true
   ssh "$host" sudo dd if=/dev/zero of=/dev/sda bs=1K count=50000 status=progress || true
+  ssh "$host" sudo sync
 done
